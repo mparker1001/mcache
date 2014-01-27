5a7f9c68abda27b39d728149f6e5d36de9500e00
Matt Parker <mparker@MPMBPR.local>
2014-01-27 13:59:18 -0600

    Fixed attribute where max memory was being evaluated as a real number

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['mcache']['listen_port'] = 11211
 default['mcache']['maxconn'] = 1024
-default['mcache']['max_memory'] = node[:memory][:total].to_i * 0.75
+max_memory = node[:memory][:total].to_i * 0.75
+default['mcache']['max_memory'] = max_memory.to_i
--- recipes/default.rb
+++ recipes/default.rb
@@ -14,12 +14,6 @@ service "memcached" do
   action [ :start, :enable ]
 end
 
-ohai "name" do
-  attribute "value" # see attributes section below
-  ...
-  action :action # see actions section below
-end
-
 template "/etc/memcached.conf" do
   source "memcached.conf.erb"
   mode "0644"
