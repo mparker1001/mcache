7b283cc18748a5c1039160ec8c9dcbd04569304b
Matt Parker <mparker@MPMBPR.local>
2014-01-30 13:45:45 -0600

    Added config hash in attributes file to allow for custom memcache config options

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,7 @@
 default['mcache']['listen_port'] = 11211
 default['mcache']['maxconn'] = 1024
 default['mcache']['max_memory'] = (node[:memory][:total].to_i * 0.75).to_i
+default['mcache']['config_options'] = {
+  '-l' => '127.0.0.1',
+  '-u' => 'memcache'
+}
